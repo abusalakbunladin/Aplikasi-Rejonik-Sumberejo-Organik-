090333d7c64032f4bba60921031677c4443b8282
gatralho-wq <gatralho@gmail.com>
2026-09-07 10:33:06 +0700

    perbaikan security.py

--- backend/app/security.py
+++ backend/app/security.py
@@ -23,7 +23,7 @@ def get_password_hash(password):
 
 def authenticate_user(db: Session, username: str, password: str):
     user = db.query(User).filter(User.username == username).first()
-    if not User:
+    if not user:
         return False
     if not verify_password(password, user.hashed_password):
         return False
