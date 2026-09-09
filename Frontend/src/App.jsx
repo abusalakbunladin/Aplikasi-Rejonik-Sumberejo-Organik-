461a94b915b1edab50baa93b3115004ffdc10054
Pixellited <alamakcihuy88@gmail.com>
2026-09-09 15:47:40 +0700

    Redesign Hero Section (Not Finished)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -45,26 +45,37 @@ function useScrollTrigger(elementId, offset = 500) {
 function Hero() {
   return (
     <div className="hero">
-      <section
-        id="home"
-        className="pt-40 pb-20 bg-green-900 relative overflow-hidden"
-      >
+      <section id="home" className="pt-90 pb-30 bg-green-900 relative overflow-hidden">
         <div className="container mx-auto relative z-2">
           <div className="w-full px-4">
-            <div className="flex flex-col lg:flex-row lg:gap-10 xl:gap-20 lg:items-end lg:justify-center md:mt-35 lg:mt-40">
+            <div className="flex flex-col lg:flex-row lg:gap-10 xl:gap-30 lg:items-end lg:justify-center md:mt-35 lg:mt-40">
               <div className="mb-10 xl:mb-0">
-                <h2 className="font-semibold text-side text-sm lg:text-lg text">
-                  Beras Organik Bersertifikat
-                </h2>
-                <h1 className="font-black text-white text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl uppercase mb-4">
-                  Sumberejo Organik
-                </h1>
+                <div className="flex flex-col">
+                  <div className="relative">
+                    <div className="w-40 h-60 bg-white/20 rounded-sm absolute top-0 -translate-x-30 -translate-y-20 -skew-x-10"></div>
 
-                <p className="max-w-md font-medium text-white text-xs mb-7 xl:text-base">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Voluptates itaque veniam dolorem eius atque ipsum in aut
-                  consectetur amet eligendi.
-                </p>
+                    <div className="flex flex-col mb-4">
+                      <h2 className="font-semibold text-side text-sm lg:text-lg">
+                        Beras Organik Bersertifikat
+                      </h2>
+
+                      <h1 className="font-black text-white text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl uppercase inline-block">
+                        Sumberejo Organik
+                      </h1>
+                    </div>
+
+                    <div className="w-150 h-55 bg-linear-to-tr from-primary/30 to-side/30 rounded-sm absolute -z-1 top-0 -translate-y-10 -translate-x-10 -skew-x-10"></div>
+
+                    <div className="w-50 h-30 bg-white/20 rounded-sm absolute right-0 -skew-x-10"></div>
+                  </div>
+                
+                  <p className="max-w-md font-medium text-white text-xs mb-7 xl:text-base">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Voluptates itaque veniam dolorem eius atque ipsum in aut
+                    consectetur amet eligendi.
+                  </p>
+                </div>
+                
 
                 <a href="#">
                   <motion.button
@@ -103,18 +114,25 @@ function Hero() {
                 </a>
               </div>
 
-              <div className="flex flex-col gap-3 sm:gap-5">
-                <div className="font-bold text-white text-xl text-center bg-side/50 rounded-lg sm:rounded-xl outline-white outline-1 p-2 select-none">
-                  <p>Pencapaian</p>
-                </div>
+                <div className="absolute xl:flex xl:flex-row xl:items-center xl:gap-2 right-0 -translate-y-20 -skew-x-10">
+                  <div className="w-3 h-15 bg-white/50 rounded-sm"></div>
+                  
+                  <div className="font-bold text-white text-3xl uppercase text-center bg-side/50 rounded-sm p-4 select-none">
+                      <p>Pencapaian</p>
+                  </div>
 
-                <div className="sm:bg-white/15 outline-white sm:outline-1 sm:shadow-xl rounded-xl sm:p-3 select-none">
-                  <div className="flex flex-col gap-2 sm:gap-5 sm:flex-row sm:items-center sm:justify-center cursor-default">
+                  <div className="w-3 h-15 bg-white/50 rounded-sm"></div>
+                </div>
+                
+
+              <div className="flex flex-col gap-3 sm:gap-5 relative">
+                <div className="sm:bg-white/15 rounded-sm sm:p-3 xl:px-8 select-none md:-skew-x-10">
+                  <div className="flex flex-col gap-2 sm:gap-5 xl:gap-10 sm:flex-row sm:items-center sm:justify-center cursor-default">
                     <div className="flex flex-col xl:gap-1 px-2 bg-white/15 rounded-lg outline-white outline-1 sm:bg-white/0 sm:outline-0 p-2 sm:p-0">
                       <p className="font-extrabold text-white text-lg sm:text-2xl md:text-3xl xl:text-4xl">
                         1.300.000
                       </p>
-                      <p className="font-semibold text-white text-xs sm:text-sm xl:text-xs">
+                      <p className="font-semibold text-white text-xs sm:text-sm xl:hidden">
                         Karung Terjual
                       </p>
                     </div>
@@ -125,7 +143,7 @@ function Hero() {
                       <p className="font-extrabold text-white text-lg sm:text-2xl md:text-3xl xl:text-4xl">
                         90%
                       </p>
-                      <p className="font-semibold text-white text-xs sm:text-sm xl:text-xs">
+                      <p className="font-semibold text-white text-xs sm:text-sm xl:hidden">
                         Kepuasan Pelanggan
                       </p>
                     </div>
@@ -136,11 +154,29 @@ function Hero() {
                       <p className="font-extrabold text-white text-lg sm:text-2xl md:text-3xl xl:text-4xl">
                         50+
                       </p>
-                      <p className="font-semibold text-white text-xs sm:text-sm xl:text-xs">
+                      <p className="font-semibold text-white text-xs sm:text-sm xl:hidden">
                         Petani Mitra
                       </p>
                     </div>
                   </div>
+                </div>
+
+                <div className="hidden xl:block xl:bg-side xl:p-1 xl:px-2 rounded-sm xl:-skew-x-10 xl:absolute xl:bottom-0 xl:-translate-x-10 xl:translate-y-7">
+                  <p className="font-semibold text-white text-xs sm:text-sm xl:text-lg">
+                      Karung Terjual
+                  </p>
+                </div>
+
+                <div className="hidden xl:block xl:bg-side xl:p-1 xl:px-2 rounded-sm xl:-skew-x-10 xl:absolute xl:bottom-0 xl:right-1/4 xl:-translate-x-13 xl:translate-y-7">
+                  <p className="font-semibold text-white text-xs sm:text-sm xl:text-lg">
+                    Kepuasan Pelanggan
+                  </p>
+                </div>
+
+                <div className="hidden xl:block xl:bg-side xl:p-1 xl:px-2 rounded-sm xl:-skew-x-10 xl:absolute xl:bottom-0 xl:right-0 xl:-translate-x-8 xl:translate-y-7">
+                  <p className="font-semibold text-white text-xs sm:text-sm xl:text-lg">
+                    Petani Mitra
+                  </p>
                 </div>
               </div>
             </div>
@@ -776,7 +812,7 @@ function About() {
                       className="bg-white rounded-xl overflow-hidden row-span-4 col-span-2"
                     >
                       <img
-                        src="/img/imagetest.jpg.jpeg"
+                        src="/img/imagetest.jpeg"
                         alt=""
                         className="w-full h-full object-cover object-center"
                       />
@@ -796,7 +832,7 @@ function About() {
                       className="bg-white rounded-xl overflow-hidden row-span-2 col-span-2"
                     >
                       <img
-                        src="/img/imagetest.jpg.jpeg"
+                        src="/img/imagetest.jpeg"
                         alt=""
                         className="w-full h-full object-cover object-center"
                       />
@@ -816,7 +852,7 @@ function About() {
                       className="bg-white rounded-xl overflow-hidden row-span-2"
                     >
                       <img
-                        src="/img/imagetest.jpg.jpeg"
+                        src="/img/imagetest.jpeg"
                         alt=""
                         className="w-full h-full object-cover object-center"
                       />
@@ -836,7 +872,7 @@ function About() {
                       className="bg-white rounded-xl overflow-hidden row-span-2"
                     >
                       <img
-                        src="/img/imagetest.jpg.jpeg"
+                        src="/img/imagetest.jpeg"
                         alt=""
                         className="w-full h-full object-cover object-center"
                       />
